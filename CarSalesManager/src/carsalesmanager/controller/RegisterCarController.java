e1e80d3a0d37d87b890e249b7c9247379e14b540
Crowlleey <georgegomees@gmail.com>
2017-11-27 22:06:56 -0200

    add color option filter

--- CarSalesManager/src/carsalesmanager/controller/RegisterCarController.java
+++ CarSalesManager/src/carsalesmanager/controller/RegisterCarController.java
@@ -5,13 +5,20 @@
  */
 package carsalesmanager.controller;
 
+import carsalesmanager.dao.ColorDAO;
+import carsalesmanager.model.Color;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
@@ -26,12 +33,27 @@ public class RegisterCarController implements Initializable {
     /**
      * Initializes the controller class.
      */
-    private Stage stage;
-   
+    @FXML
+    private ComboBox CBColor;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        populateCBColor();
     }    
+    
+    
+    private void populateCBColor(){
+        ColorDAO cDao = new ColorDAO(new Color());
+        ArrayList<Color> colors =  (ArrayList<Color>) cDao.findAll();
+        ArrayList<String> colorNames = new ArrayList<>();
+        
+        for (Color color : colors) {
+            colorNames.add(color.getName());
+        }
+      
+        ObservableList obsCores = FXCollections.observableList(colorNames);
+        CBColor.getItems().clear();
+        CBColor.setItems(obsCores);
+    }
     
 }
